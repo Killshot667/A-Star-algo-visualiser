feee2c524a077fa704416ab8042714206f2579db
Shabari S Nair <shabarisnair123@gmail.com>
2020-09-21 12:30:25 +0530

    working model completed

--- a-start.py
+++ a-start.py
@@ -92,9 +92,63 @@ class Spot:
         return False
     
 def h(a, b):
-    x1, y1 = a
-    x2, y2 = b
-    return abs(x1-x2) + abs(y1-y2)
+    row1, col1 = a
+    row2, col2 = b
+    return abs(row1-row2) + abs(col1-col2)
+
+def reconstruct_path(came_from, current, draw):
+    while current in came_from:
+        current = came_from[current]
+        current.make_path()
+        draw()
+
+def algorithm(draw, grid, start, end):
+    count = 0
+    open_set = PriorityQueue()
+    open_set.put((0,count,start))
+    came_from = {}
+    visited=set()
+    g_score = {spot: float("inf") for row in grid for spot in row}
+    g_score[start] = 0
+    f_score = {spot: float("inf") for row in grid for spot in row}
+    f_score[start] = h(start.get_pos(), end.get_pos())
+    open_set_hash = {start}
+
+    while not open_set.empty():
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+        
+        current = open_set.get()[2]
+        visited.add(current)
+        open_set_hash.remove(current)
+        if current == end:
+            reconstruct_path(came_from, end, draw)
+            end.make_end()
+            start.make_start()
+            return True
+        
+        for neighbor in current.neighbors:
+            if neighbor not in visited:
+                temp_g_score = g_score[current] + 1;
+                if temp_g_score < g_score[neighbor]:
+                    came_from[neighbor] = current
+                    g_score[neighbor] = temp_g_score
+                    f_score[neighbor] = temp_g_score + h(neighbor.get_pos(), end.get_pos())
+                    if neighbor not in open_set_hash:
+                        count += 1
+                        open_set.put((f_score[neighbor], count, neighbor))
+                        open_set_hash.add(neighbor)
+                        neighbor.make_open()
+        
+        draw()
+        if current != start:
+            current.make_closed()
+    
+    return False
+
+
+        
 
 def make_grid(rows, span):
     gap = span // rows
@@ -179,8 +233,17 @@ def main(win, span):
                     end = None
             
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE and not started:
-                    pass
+                if event.key == pygame.K_SPACE and start and end:
+                    for row in grid:
+                        for spot in row:
+                            spot.update_neighbors(grid)
+                    
+                    algorithm(lambda: draw(win, grid, ROWS, span), grid, start, end)
+                
+                if event.key == pygame.K_c:
+                    start = None
+                    end = None
+                    grid = make_grid(ROWS, WIDTH)
    
     pygame.quit()       
     sys.exit()
